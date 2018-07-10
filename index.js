61103b74ae6c501529ba9b98edd24ba617c0344f
Rahul <vermarahulkps@gmail.com>
2018-07-10 14:21:32 +0530

    First commit

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+const set = new Map(englishToBrailleLiteralSet);
+
+function converter() {
+  const str = document.getElementById('sourceLangText').value;
+  let res = '';
+  let i = 0;
+  for (i = 0; i < str.length; i += 1) {
+    res += set.get(str[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = res;
+}
+
+window.onload = function () {
+  const btn = document.getElementById('btnConvertEnglishToBraille');
+  btn.addEventListener('click', converter);
+};
